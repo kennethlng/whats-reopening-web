487806e5e2a060a545e25ff9cabbb87871ce3e43
kennethlng <ng.kennethlee@gmail.com>
2020-06-21 20:21:45 -0700

    Moved firebase config into its own file

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,17 +1,7 @@
 import firebase from 'firebase'; 
+import { config } from '../config/firebase'; 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCGNMrxkBiJo-cja1XOWJJV_JmpWMFd4Pc",
-    authDomain: "trip-479ef.firebaseapp.com",
-    databaseURL: "https://trip-479ef.firebaseio.com",
-    projectId: "trip-479ef",
-    storageBucket: "trip-479ef.appspot.com",
-    messagingSenderId: "819578427882",
-    appId: "1:819578427882:web:1da6bc2c75bb3d43467a93",
-    measurementId: "G-5K927XZWEF"
-};
-
-firebase.initializeApp(firebaseConfig); 
+firebase.initializeApp(config); 
 const db = firebase.firestore(); 
 
 export { db };
