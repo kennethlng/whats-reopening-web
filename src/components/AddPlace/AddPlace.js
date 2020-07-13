d3cf92c922984408a43d090425583af806763b46
kennethlng <ng.kennethlee@gmail.com>
2020-07-13 13:27:32 -0700

    StatusSelect component for AddPlace page

--- src/components/AddPlace/AddPlace.js
+++ src/components/AddPlace/AddPlace.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { AddPlaceContext } from './context';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField'; 
+import StatusSelect from './StatusSelect';
 
-const AddPlace = () => (
-    <div>
-        <h1>Add place</h1>
-    </div>
-)
+const styles = theme => ({
+    root: {
 
-export default AddPlace;
+    }
+})
+
+class AddPlace extends React.Component {
+    state = {
+        name: '',
+        status: '',
+        openingDate: '',
+        isAffiliated: false,
+        affiliateName: '',
+        affiliateEmail: '',
+        notes: ''
+    }
+
+    update = (name) => {
+        this.setState({
+            name
+        })
+    }
+
+    handleSubmit = () => {
+
+    }
+
+    render() {
+        const { classes } = this.props; 
+        const { name, status, openingDate, isAffiliated, affiliateName, affiliateEmail } = this.state; 
+
+        return (
+            <AddPlaceContext.Provider 
+                value={{ 
+                    name, 
+                    status, 
+                    openingDate, 
+                    isAffiliated, 
+                    affiliateName, 
+                    affiliateEmail, 
+                    update: this.update, 
+                    updateStatus: (status) => this.setState({ status })  
+                }}
+            >
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField variant="outlined" label="Place name" required onChange={(e) => this.setState({ name: e.target.value })}/>
+                    <StatusSelect/>
+                </form>
+            </AddPlaceContext.Provider>
+        )
+    }
+}
+
+export default withStyles(styles)(AddPlace);
