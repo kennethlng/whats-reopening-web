4ae9239a3fb0327b46ea93e9548df22fa14e7d6d
kennethlng <ng.kennethlee@gmail.com>
2020-07-20 15:27:26 -0700

    Updated Status component with tooltip

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as STATUS from '../../constants/status'; 
 import Chip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles(theme => ({
     [STATUS.OPEN.ID]: {
@@ -23,14 +24,34 @@ export default function Status(props) {
 
     switch(props.status) {
         case STATUS.OPEN.ID: 
-            return <Chip label={STATUS.OPEN.TITLE} className={classes[STATUS.OPEN.ID]}/>;
+            return (
+                <Tooltip title={STATUS.OPEN.DESCRIPTION}>
+                    <Chip label={STATUS.OPEN.TITLE} className={classes[STATUS.OPEN.ID]}/>
+                </Tooltip>
+            )
         case STATUS.LIMITED_OPEN.ID: 
-            return <Chip label={STATUS.LIMITED_OPEN.TITLE} className={classes[STATUS.LIMITED_OPEN.ID]}/>;
+            return (
+                <Tooltip title={STATUS.LIMITED_OPEN.DESCRIPTION}>
+                    <Chip label={STATUS.LIMITED_OPEN.TITLE} className={classes[STATUS.LIMITED_OPEN.ID]}/>
+                </Tooltip>
+            )
         case STATUS.TEMPORARILY_CLOSED.ID: 
-            return <Chip label={STATUS.TEMPORARILY_CLOSED.TITLE} className={classes[STATUS.TEMPORARILY_CLOSED.ID]}/>;
+            return (
+                <Tooltip title={STATUS.TEMPORARILY_CLOSED.DESCRIPTION}>
+                    <Chip label={STATUS.TEMPORARILY_CLOSED.TITLE} className={classes[STATUS.TEMPORARILY_CLOSED.ID]}/>
+                </Tooltip>
+            )
         case STATUS.PERMANENTLY_CLOSED.ID: 
-            return <Chip label={STATUS.PERMANENTLY_CLOSED.TITLE} className={classes[STATUS.PERMANENTLY_CLOSED.ID]}/>;
+            return (
+                <Tooltip title={STATUS.PERMANENTLY_CLOSED.DESCRIPTION}>
+                    <Chip label={STATUS.PERMANENTLY_CLOSED.TITLE} className={classes[STATUS.PERMANENTLY_CLOSED.ID]}/>
+                </Tooltip>
+            )
         default: 
-            return <Chip label={STATUS.TEMPORARILY_CLOSED.TITLE} className={classes[STATUS.TEMPORARILY_CLOSED.ID]}/>;
+            return (
+                <Tooltip title="More information is needed">
+                    <Chip label={Uncertain} />
+                </Tooltip>
+            )
     }
 }
--- src/constants/status.js
+++ src/constants/status.js
@@ -1,20 +1,24 @@
 export const OPEN = {
     ID: "open",
-    TITLE: "Open"
+    TITLE: "Open",
+    DESCRIPTION: "This place is fully open"
 } 
 
 export const LIMITED_OPEN = {
     ID: "limitedOpen",
-    TITLE: "Open but limited in capacity"
+    TITLE: "Open but limited in capacity",
+    DESCRIPTION: "This place only allows a certain number of customers"
 }
 
 export const TEMPORARILY_CLOSED = {
     ID: "temporarilyClosed",
-    TITLE: "Closed temporarily"
+    TITLE: "Closed temporarily",
+    DESCRIPTION: "This place is closed for the time-being but will open up again on the reopening day."
 } 
 export const PERMANENTLY_CLOSED = {
     ID: "permanentlyClosed",
-    TITLE: "Permanently closed"
+    TITLE: "Permanently closed",
+    DESCRIPTION: "This place is no longer operating"
 }
 
 export const OTHER = {
